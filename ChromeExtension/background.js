829c96d87bd67dbda62e736f48e535fe35e8d799
Rishav Anand <rishva343@gmail.com>
2022-02-25 00:14:03 +0530

    url and time input + create alarm has been set to work

--- ChromeExtension/background.js
+++ ChromeExtension/background.js
@@ -1,17 +1,22 @@
 
-chrome.runtime.onInstalled.addListener(() => {});
+chrome.runtime.onInstalled.addListener(() => {
+
+});
+
+let url = "https://meet.google.com/qfc-zsjy-tzd";
+chrome.storage.local.get(['mUrl'], function (res) {
+  url = res.mUrl;
+});
 
 chrome.alarms.onAlarm.addListener(function (alarm) {
 
-  let url = "https://meet.google.com/qfc-zsjy-tzd";
-
   chrome.tabs.create({ url });
 
-  let [tab] =  chrome.tabs.query({ active: true, currentWindow: true });
+  let [tab] = chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
-          target: { tabId: tab.id },
-          files: ["content-script.js"]
-        });
+    target: { tabId: tab.id },
+    files: ["content-script.js"]
+  });
   console.log("Got an alarm!", alarm);
 });
