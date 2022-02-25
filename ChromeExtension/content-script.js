6fe368b60b8ecffaffd5f43dfac4e1404cf414e1
Mohit <mohitbro96@gmail.com>
2022-02-25 21:04:37 +0530

    meeting ending after specific time

--- ChromeExtension/content-script.js
+++ ChromeExtension/content-script.js
@@ -10,19 +10,42 @@
 // let on_meet_mic_OnOff = document.getElementsByClassName('VfPpkd-Bz112c-LgbsSe')[0];
 // let on_meet_cam_OnOff = document.getElementsByClassName('VfPpkd-Bz112c-LgbsSe')[1];
 
-
 join_meeting();
 
 function join_meeting() {
-const delay = ms => new Promise(res => setTimeout(res, ms));
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+
     delay(500);
     let joinNow = document.querySelectorAll('.NPEfkd')[0];
     let mic_OnOff = document.getElementsByClassName('U26fgb')[0];
     let cam_OnOff = document.getElementsByClassName('U26fgb')[1];
-    
-    mic_OnOff.click();
-    delay(100);
-    cam_OnOff.click();
-    delay(100);
-    joinNow.click();
+
+    try {
+
+        mic_OnOff.click();
+        delay(100);
+        cam_OnOff.click();
+        delay(100);
+        joinNow.click();
+
+
+    }
+    catch (e) {
+        console.log(e);
+    }
+
+    setTimeout(() => {
+        end_meeting();
+    }, 30000); // meeting ending time
+}
+
+function end_meeting() {
+
+    let end_meet = document.getElementsByClassName('VfPpkd-Bz112c-LgbsSe')[5];
+
+    try {
+        end_meet.click();
+    } catch (e) {
+        console.log(e);
+    }
 }
